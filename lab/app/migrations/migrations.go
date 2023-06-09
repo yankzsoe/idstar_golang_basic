b9e0cffd2f46abef03df53ec40448ff43e173931
yankzsoe <yankzsoe@gmail.com>
2023-06-09 17:36:28 +0700

    cleanup migrations

--- lab/app/migrations/migrations.go
+++ lab/app/migrations/migrations.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Migrator(db *gorm.DB) error {
-	// Auto create table if object is't exist
+	// Auto create table if user object is't exist
 	user := models.UserModel{}
 	if !db.Migrator().HasTable(user.TableName()) {
 		db.AutoMigrate(user)
@@ -22,35 +22,5 @@ func Migrator(db *gorm.DB) error {
 		}
 	}
 
-	// cl, _ := db.Migrator().ColumnTypes(&user)
-
-	// for _, c := range cl {
-	// 	if c.Name() == "ftest" {
-	// 		mg := db.Migrator()
-	// 		sf := &schema.Field{
-	// 			Name:     "ftests",
-	// 			DataType: "string",
-	// 			Size:     60,
-	// 			// DBName:   db.Migrator().CurrentDatabase(),
-	// 			DBName:            "ftest",
-	// 			Schema:            db.Statement.Schema,
-	// 			IndirectFieldType: reflect.TypeOf(user.FieldTest),
-	// 		}
-	// 		err := mg.MigrateColumn(&user, sf, c)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 			return err
-	// 		}
-	// 	}
-	// }
-
-	// migrator.Migrate(&models.UserModel{}, func(tx *gorm.DB) error {
-	// 	// Menjalankan pernyataan SQL untuk mengubah tipe data kolom
-	// 	if err := tx.Exec("ALTER TABLE users ALTER COLUMN column_name TYPE new_data_type").Error; err != nil {
-	// 		return err
-	// 	}
-
-	// 	return nil
-	// })
 	return nil
 }
